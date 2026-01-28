a8c2e7950333bc6669104e7cfd9c1dd834d95cb1
teach-arrowcloud <teach-arrowcloud@gmail.com>
2026-01-28 11:00:16 -0500

    chore: update settings.json

---
+++ classes/01-28 W/today.py
@@ -0,0 +1,78 @@
+'''
+==
+!=
+>
+<
+<=
+>=
+
+not 
+in
+and 
+or
+if x == 5 and y == 7:
+
+
+    x==5     y==7      and      or
+    T         T         T        T
+    T         F         F        T
+    F         T         F        T
+    F         F         F        F
+'''
+x = 7
+y = 2
+
+if x:
+    print(x)
+
+if not y :
+    print(y)
+
+'''
+x=5
+x==5
+x!=56
+x < 10
+x >=5
+'''
+
+'''
+if x > y:
+   print('x is bigger)
+else:
+   print('y is bigger)
+
+x = 10
+if x < 0:
+    print('negative')
+elif x > 0:
+    print('positive')
+else x == 0:
+    print('zero')
+
+grade = 70
+
+
+x = 5
+if x == 5:
+    print('yes')
+
+s = ''
+if  s :
+    print('empty')
+
+s= ' '
+if s :
+    print(s,'space')
+'''
+x = input('Please enter a integer: ')
+print(x)
+
+x = 5
+y = 7
+
+temp = x
+x = y
+y = temp
+
+x,y = y,x
